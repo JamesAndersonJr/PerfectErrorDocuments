65a514d63742ebb02192bfde72d0676bd5bbfb5e
James Anderson Jr <jamesaandersonjr@yahoo.com>
2017-06-09 01:27:36 -0400

    Revert "Updated protocol (SSL/TLS/HTTPS)"
    This reverts commit c03a029e107382dddfaebbdb8ce909a2c1dcb763.

--- error_documents/includes/domain_info.php
+++ error_documents/includes/domain_info.php
@@ -58,37 +58,37 @@ if (($sub_domain == "")||( $sub_domain == null ))
 
 /* Declare domain-related variables. [BEGIN] */
 
-/* Get the full domain name of the primary website (e.g. [ https://www.example.com ]). [BEGIN] */
+/* Get the full domain name of the primary website (e.g. [ http://www.example.com ]). [BEGIN] */
 
 $primary_Website_Domain_Name = $protocol."www.".$server_url;
 
-/* Get the full domain name of the primary website (e.g. [ https://www.example.com ]). [END] */
+/* Get the full domain name of the primary website (e.g. [ http://www.example.com ]). [END] */
 
-/* Get the full domain name of the current website (e.g. [ https://current-3ld.example.com ]). [BEGIN] */
+/* Get the full domain name of the current website (e.g. [ http://current-3ld.example.com ]). [BEGIN] */
 
 $current_Website_Domain_Name = $protocol.$sub_domain.".".$server_url;
 
-/* Get the full domain name of the current website (e.g. [ https://current-3ld.example.com ]). [END] */
+/* Get the full domain name of the current website (e.g. [ http://current-3ld.example.com ]). [END] */
 
-/* Get the complete URL address to the current web page, on the Internet (e.g. [ https://www.example.com/index.php ]). [BEGIN] */
+/* Get the complete URL address to the current web page, on the Internet (e.g. [ http://www.example.com/index.php ]). [BEGIN] */
 
 $current_Webpage_Complete_URL_Address = $current_Website_Domain_Name.$current_path;
 
-/* Get the complete URL address to the current web page, on the Internet (e.g. [ https://www.example.com/index.php ]). [END] */
+/* Get the complete URL address to the current web page, on the Internet (e.g. [ http://www.example.com/index.php ]). [END] */
 
-/* Get the canonical URL address to the current web page, on the Internet (e.g. [ https://www.example.com/page.php ], or [ https://www.example.com ], but NOT [ https://www.example.com/index.php ], NOR [ https://www.example.com/ ]). [BEGIN] */
+/* Get the canonical URL address to the current web page, on the Internet (e.g. [ http://www.example.com/page.php ], or [ http://www.example.com ], but NOT [ http://www.example.com/index.php ], NOR [ http://www.example.com/ ]). [BEGIN] */
 
 $current_Webpage_Canonical_URL_Address = preg_replace('/((\\index)\.[a-z]+)$/', '', $current_Webpage_Complete_URL_Address); /* Removes any known homepage path of the URL address, but NOT normal page paths. */
 
 $current_Webpage_Canonical_URL_Address = preg_replace('/\/$/', '', $current_Webpage_Canonical_URL_Address); /* Removes any trailing forward slash ("/") from the end of the URL. */
 
-/* Get the canonical URL address to the current web page, on the Internet (e.g. [ https://www.example.com/page.php ], or [ https://www.example.com ], but NOT [ https://www.example.com/index.php ], NOR [ https://www.example.com/ ]). [END] */
+/* Get the canonical URL address to the current web page, on the Internet (e.g. [ http://www.example.com/page.php ], or [ http://www.example.com ], but NOT [ http://www.example.com/index.php ], NOR [ http://www.example.com/ ]). [END] */
 
-/* Get the primary websites version of the current page (e.g. used at [ https://m.example.com/about.php ], would produce [ https://www.example.com/about.php ]). Typically used for canonical links on mobile-only sub-domains. [BEGIN] */
+/* Get the primary websites version of the current page (e.g. used at [ http://m.example.com/about.php ], would produce [ http://www.example.com/about.php ]). Typically used for canonical links on mobile-only sub-domains. [BEGIN] */
 
 $primary_Website_Version = $primary_Website_Domain_Name.$current_path;
 
-/* Get the primary websites version of the current page (e.g. used at [ https://m.example.com/about.php ], would produce [ https://www.example.com/about.php ]). Typically used for canonical links on mobile-only sub-domains. [END] */
+/* Get the primary websites version of the current page (e.g. used at [ http://m.example.com/about.php ], would produce [ http://www.example.com/about.php ]). Typically used for canonical links on mobile-only sub-domains. [END] */
 
 /* Declare domain-related variables. [END] */
 
