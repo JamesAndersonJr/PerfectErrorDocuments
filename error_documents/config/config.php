1ad28aeac5ce90699cfacf5390011487e3bbe59b
James Anderson Jr <jamesaandersonjr@yahoo.com>
2017-06-09 09:52:48 -0400

    Revert "New Version (1.7.2)! * MAJOR UPDATE *"
    This reverts commit 36f70b63cc03b253018ffcfc1604f7cc3dc2fffb.

--- error_documents/config/config.php
+++ error_documents/config/config.php
@@ -2,16 +2,16 @@
 
 /* Perfect Error Documents Version. - DO NOT EDIT [BELOW] */
 
-$ped_version = "1.7.2";
+$ped_version = "1.7.1";
 
 /* Perfect Error Documents Version. - DO NOT EDIT [ABOVE] */
 
-/* Edit [BELOW], if necessary, to customize the script. */
+/* Edit [BELOW], if necessary, to customize script. */
 
 # Enter your webmasters contact URL address below. It can be a relative URL, an absolute URL, or a 'MailTo:' email link (e.g. $webmaster_link = "/contact_webmaster.php";). 
 
 $webmaster_link = "/contact_webmaster.php";
 
-/* Edit [ABOVE], if necessary, to customize the script. */
+/* Edit [ABOVE], if necessary, to customize script. */
 
 ?>
--- error_documents/includes/royalty.php
+++ error_documents/includes/royalty.php
@@ -2,17 +2,17 @@
 
 This web-page is part of the 'Perfect Error Documents' v.<?php echo $ped_version; ?> online script.
 
-A free copy of the 'Perfect Error Documents' online script can be found here: [ https://perfecterrordocs.com/download/ ].
+A free copy of the 'Perfect Error Documents' online script can be found here: [ http://perfecterrordocs.com/download/ ].
 
-You may make a voluntary donation for the developer(s) of the script here: [ https://perfecterrordocs.com/donate/ ].
+You may make a voluntary donation for the developer(s) of the script here: [ http://perfecterrordocs.com/donate/ ].
 
-For more information, documentation, and additional download links to all future versions of this script, please visit us at: [ https://perfecterrordocs.com ].
+For more information, documentation, and additional download links to all future versions of this script, please visit us at: [ http://perfecterrordocs.com ].
 
 'Perfect Error Documents' online script is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
 
-of MERCHANTABILITY, or FITNESS FOR A PARTICULAR PURPOSE. See the full license agreement at [ https://perfecterrordocs.com/license/ ].
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the full license agreement at [ http://perfecterrordocs.com/license/ ].
 
-You should have received a PDF copy of the license agreement, along with your download of 'Perfect Error Documents' online script. If not, please visit: [ https://perfecterrordocs.com/license/ ], for your free copy.
+You should have received a PDF copy of the license agreement, along with your download of 'Perfect Error Documents' online script. If not, please visit: [ http://perfecterrordocs.com/license/ ], for your free copy.
 
 For support, and all other inquiries, please feel free to email us at: support [at] perfecterrordocs [dot] com.
 
